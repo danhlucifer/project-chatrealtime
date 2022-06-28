f01acb0e6b975c3043f41de411b087a4fd9f78cc
danhlucifer <danhit3003@gmail.com>
2022-06-28 11:14:13 +0700

    upload new code

--- back-end/server.js
+++ back-end/server.js
@@ -14,6 +14,7 @@ app.use('/users', userRoutes);
 
 //link MongoDB
 const mongoose = require('mongoose');
+const { socket } = require("../front-end/src/context/appContext");
 require('dotenv').config();
 
 mongoose.connect(process.env.MONGODB_URL, ()=>{
@@ -27,6 +28,21 @@ const io = require('socket.io')(server, {
         methods: ['GET', 'POST']
     }
 })
+
+// app.get('/rooms', (req, res) =>{
+//     res.json(rooms)
+// })
+
+// //socket connection
+// io.on('connection',(socket) =>{
+
+//     socket.on('join-room', async(room)=>{
+//         socket.join(room)
+//     })
+
+
+// })
+
 server.listen(process.env.PORT,()=>{
     console.log(`Lisening to port: ${process.env.PORT}`);
 })
