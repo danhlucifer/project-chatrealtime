55835c04101fd63a4b10735068d3efccb32ace2e
danhlucifer <danhit3003@gmail.com>
2022-07-19 09:40:44 +0700

    done

--- back-end/connection.js
+++ back-end/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect("mongodb+srv://intern-chatapp:nodejs@cluster0.scequsk.mongodb.net/?retryWrites=true&w=majority", ()=> {
+mongoose.connect("mongodb+srv://chat-app:nodejs@cluster0.h84v5i5.mongodb.net/?retryWrites=true&w=majority", ()=> {
   console.log('connected to mongodb')
 })
